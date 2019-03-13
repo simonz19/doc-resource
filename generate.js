fe3448db084215e88fa7e5cade7788347d53c184
zouxiaoya <576490177@qq.com>
2019-03-13 19:06:01 +0800

    auto generate

---
+++ generate.js
@@ -0,0 +1,40 @@
+const recursive = require("recursive-readdir");
+const prettier = require("prettier");
+const { join } = require("path");
+const fs = require("fs");
+
+const srcRootName = "doc";
+
+recursive(join(__dirname, srcRootName), function(err, files) {
+  const fileTreeFlat = {};
+  files.forEach(file => {
+    file = file.replace(/\\/g, "/");
+    let fileArr = file.split("/");
+    if (fileArr.every(fileLeaf => fileLeaf !== "private")) {
+      fileArr = fileArr.slice(fileArr.indexOf(srcRootName));
+      const lastIndex = fileArr.length - 1;
+      const categoryName = fileArr.slice(0, lastIndex).join("/");
+      if (!fileTreeFlat[categoryName]) {
+        fileTreeFlat[categoryName] = [];
+      }
+      fileTreeFlat[categoryName].push(fileArr[lastIndex]);
+    }
+  });
+
+  let fileText = "# Summary\n\n";
+  fileText += "- [Introduction](README.md)\n";
+
+  for (const key in fileTreeFlat) {
+    if (fileTreeFlat.hasOwnProperty(key)) {
+      fileText += `- ${key}\n`;
+      fileTreeFlat[key].forEach(file => {
+        fileText += `  - [${file.split(".")[0]}](${key}/${file})\n`;
+      });
+    }
+  }
+
+  fs.writeFile(join(__dirname, "./SUMMARY.md"), fileText, err => {
+    if (err) throw err;
+    console.log("generate success");
+  });
+});
